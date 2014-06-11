ce24fe2ad4ae90c79e4aee36aaf9e7fc093b498d
Samuli Ulmanen <samuli.ulmanen@rovio.com>
2014-06-11 14:59:35 +0300

    use timeout service to draw component

--- src/select2.js
+++ src/select2.js
@@ -191,7 +191,9 @@ angular.module('ui.select2', []).value('uiSelect2Config', {}).directive('uiSelec
         if(attrs.uiSelect2) {
             scope.$watch(attrs.uiSelect2, function(newVal) {
                 if(newVal) {
-                elm.select2(angular.extend({}, options, scope.$eval(attrs.uiSelect2)));
+                    $timeout(function() {
+                        elm.select2(angular.extend({}, options, scope.$eval(attrs.uiSelect2)));
+                    });
                 }
             });
         }
